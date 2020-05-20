9f07c710fcb4a2d1e9ba2b58715a2eb1e159e770
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-05-20 08:42:33 +0530

    code corrected

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const axios = require('axios')
 async function getTotalPriceInBaseCurrency (text,currency,date) {
     let total=0;
     let conversionRates;
-    text=text.replace('$','USD');
-    text=text.replace('€','EUR');
-    text=text.replace('₹','INR');
+    text=text.replace(/\$/g,'USD');
+    text=text.replace(/€/g,'EUR');
+    text=text.replace(/₹/g,'INR');
+    // console.log(text);
     let result=text.match(/([A-Z]{3}\s*)([\d,.]*)/g);
+    // console.log(result);
     let currencyArray=[];
     for(let i=0;i<result.length;i++){
         let temp=[];
@@ -34,3 +36,10 @@ rice packets ₹700
 Washing machine AUD1,286.22
 cashew nuts CHF48
 4 motorcycles IDR 2,600,000`, "AUD", '2020-02-16').then((data)=>console.log(data));
+getTotalPriceInBaseCurrency(`apples $16.00
+orange juice €4
+rice packets ₹700
+Nuts packets ₹1500
+Washing machine AUD1,286.22
+cashew nuts CHF48
+4 motorcycles IDR 2,600,000`, "AUD", '2020-02-16').then((data)=>console.log(data));;
